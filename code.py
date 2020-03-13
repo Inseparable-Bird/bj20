83f909effc76aa563c65c321ddb64b474ed50ad9
Inseparable-Bird <727106206@qq.com>
2020-03-13 18:20:12 +0800

    添加login视图

--- code.py
+++ code.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-  
+from booktest.models import User
+
     def index(request):
         return HttpResponse("首页")
 
+    def login(request):
+        username = request.POST.get('username')
+        return HttpResponse("登录成功")
